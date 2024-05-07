74a8102a639bfc7e2b780f7bf6061a11c053abe6
Miguel Angel <miguelangelguaivi@gmail.com>
2024-05-07 21:41:40 +0200

    feat: add a redirection to login when creating petition
    to stop non-logged users to create petitions (although they are not able from backend), they are redirected to login page to make sure they are logged before they can create a petition

--- src/app/petition/petition-list/petition-list.component.ts
+++ src/app/petition/petition-list/petition-list.component.ts
@@ -4,13 +4,15 @@ import { Petition } from '../model/Petition';
 import { PetitionService } from '../petition.service';
 import { MatDialog } from '@angular/material/dialog';
 import { PetitionEditComponent } from '../petition-edit/petition-edit.component';
+import { AuthService } from 'src/app/core/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-petition-list',
   templateUrl: './petition-list.component.html',
   styleUrls: ['./petition-list.component.scss']
 })
-export class PetitionListComponent implements OnInit{
+export class PetitionListComponent implements OnInit {
 
 
   dataSource = new MatTableDataSource<Petition>();
@@ -19,6 +21,8 @@ export class PetitionListComponent implements OnInit{
   constructor(
     private petitionService: PetitionService,
     public dialog: MatDialog,
+    private authService: AuthService,
+    private router: Router,
   ) { }
 
   ngOnInit(): void {
@@ -28,12 +32,17 @@ export class PetitionListComponent implements OnInit{
     );
   }
   createPetition() {
-    const dialogRef = this.dialog.open(PetitionEditComponent, {
-      data: {}
-    });
+    if (this.authService.isAuthenticated()) {
 
-    dialogRef.afterClosed().subscribe(result => {
-      this.ngOnInit();
-    });
+      const dialogRef = this.dialog.open(PetitionEditComponent, {
+        data: {}
+      });
+
+      dialogRef.afterClosed().subscribe(result => {
+        this.ngOnInit();
+      });
+    } else{
+      this.router.navigate(['/auth/signin']);
+    }
   }
 }
